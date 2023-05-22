0165be08a869d08df7b51679782b9b4d8898de34
Daniel Rice <danielrice5627@gmail.com>
2023-05-22 13:55:00 -0700

    commit tree ffacb39014c978b6e93ee7df4c4409b251ca6bad

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+#GET /items
+def index
+    render json: Item.all, include: :user, status: :ok
+end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+#GET /users/:id
+def show
+    render json: User.find(params[:id]), include: :items, status: :ok
+end
 end
